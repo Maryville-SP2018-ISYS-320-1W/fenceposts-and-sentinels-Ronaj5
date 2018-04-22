67115152cfe4d861cb5f4ab4bacac2da37cc5ca7
Ronaj5 <37316864+Ronaj5@users.noreply.github.com>
2018-04-22 13:22:31 -0500

    Fenceposts
    homework FindMinMax

--- src/P1_FindMinAndMax.java
+++ src/P1_FindMinAndMax.java
@@ -1,3 +1,4 @@
+
 /*
 	ISYS 320
 	Name(s):
@@ -9,18 +10,31 @@ import java.util.Scanner;
 public class P1_FindMinAndMax {
 
 	public static void main(String[] args) {
-        Scanner console = new Scanner( System.in );
-        
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        
-        // YOUR CODE STARTS HERE
+		Scanner console = new Scanner(System.in);
 
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
 
-        // YOUR CODE ENDS HERE
-        
-        System.out.println( "Maximum was " + max );
-        System.out.println( "Minimum was " + min );
-    }
+		// YOUR CODE STARTS HERE
+		int SENTINEL = -1;
+		System.out.print("Type a number (or " + SENTINEL + " to stop): ");
+
+		int input = console.nextInt();
+
+		while (input != SENTINEL) {
+			if (input < min) {
+				min = input;
+			} else if (input > max) {
+				max = input;
+			}
+
+			System.out.print("Type a number (or " + SENTINEL + " to stop): ");
+			input = console.nextInt();
+		}
+		// YOUR CODE ENDS HERE
+
+		System.out.println("Maximum was " + max);
+		System.out.println("Minimum was " + min);
+	}
 
 }
